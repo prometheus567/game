0dca4ab7810c8270a0d3d66b15f8a4a7726895a7
prometheus <haonvn.24ce@vku.udn.vn>
2024-12-18 16:29:38 +0700

    Delete StoryManager.java

--- StoryManager.java
+++
@@ -1,9 +0,0 @@
-public class StoryManager {
-    public static String getVillageStory() {
-        return "Kane được dân làng cứu sống, nhưng những cơn ác mộng về ký ức lại ám ảnh anh.";
-    }
-
-    public static String getAdventureStory() {
-        return "Kane rời làng, đối mặt với những nguy hiểm để tìm lại quá khứ.";
-    }
-}
